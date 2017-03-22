59ffb2308cb54a6bd28f15bc2869a12858b8b29e
cosmologist-lys <lyskaka22@qq.com>
2017-03-22 17:34:38 +0800

    add global variable

--- routes/blueprint.js
+++ routes/blueprint.js
@@ -7,12 +7,11 @@ router.use(function (req, res, next) {
 	if (!req.session.user) res.redirect('/');
 	next();
 });
-
+global.flg.blue = 'blue';
 router.get('/index',function (req, res) {
 	var user = req.session.user;
 	var time = new Date().toLocaleDateString();
-	var flg = 'blue';
-	res.render('blueprint/bpindex',{flg:flg,time:time,user:user})
+	res.render('blueprint/bpindex',{flg:global.flg.blue,time:time,user:user})
 });
 
 module.exports = router;
